d48924475e735d0e88b89020433fb12838ed59e5
sparab2 <sparab2@visionlab36-pc.uncc.edu>
2025-07-28 23:58:00 -0400

    updated sync header, with proper data and column headers for all the plugins

--- include/sync_utils.h
+++ include/sync_utils.h
@@ -11,10 +11,16 @@ inline uint32_t nextEventId() {
 }
 
 #ifndef SYNC_CSV_PATH
-#define SYNC_CSV_PATH "/home/md2/sparab2/wind/uncc_wind_control/ros_image/ros_ws/sync_log.csv"
+#define SYNC_CSV_PATH "/home.md2/sparab2/wind/uncc_wind_control/ros_image/ros_ws/sync_log.csv"
 #endif
 
 namespace blast3d_sync {
+inline struct CSVFileInit {
+    CSVFileInit() {
+        std::ofstream f(SYNC_CSV_PATH, std::ios::out);  // Overwrite file!
+        f << "event_id,source,sim_time,ros_time,latency_ms,vehicle,standoff_dist,seq\n";
+    }
+} _csv_file_init_once;
 
 inline uint32_t nextEventId() {
     static std::atomic<uint32_t> id{1};
@@ -51,12 +57,13 @@ inline void publishSyncLog(ros::Publisher& pub,
     pub.publish(msg);
 
     std::lock_guard<std::mutex> lk(csvMutex());
-    static bool wrote_header = false;
+//    static bool wrote_header = false;
     auto& f = csvStream();
-    if (!wrote_header) {
-        f << "event_id,source,sim_time,ros_time,latency_ms,vehicle,standoff_dist,seq\n";
-        wrote_header = true;
-    }
+//    if (!wrote_header) {
+//        f << "event_id,source,sim_time,ros_time,latency_ms,vehicle,standoff_dist,seq\n";
+//        wrote_header = true;
+//    }
+    f << std::fixed << std::setprecision(9);
     const double latency_ms = (msg.ros_time - msg.sim_time) * 1e3;
     f << msg.event_id << ',' << msg.source << ',' << msg.sim_time << ','
       << msg.ros_time << ',' << latency_ms << ',' << msg.vehicle << ','
